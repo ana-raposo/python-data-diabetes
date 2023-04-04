61d9a07ee7b56f1727ea55c6d44dbf20387b0658
ana-raposo <124524866+ana-raposo@users.noreply.github.com>
2023-04-05 00:02:17 +0100

    corr matrix, boxplots, violinplots, conclusions

--- Diabetes.py
+++ Diabetes.py
@@ -12,6 +12,7 @@ import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
 
+#import diabetes.csv file
 stats = pd.read_csv('diabetes.csv')
 
 """
@@ -61,17 +62,17 @@ high_bloodpressure_diabetes = len(stats[(stats.BloodPressure>70) & (stats.Outcom
 """
 DATA VISUALIZATION
 """
-
-stats.Outcome = stats.Outcome.astype('category')
 #col = list(stats.columns[0:8])
 plt.rcParams['axes.grid'] = True
 plt.rcParams.update({'font.size': 12})
+sns.set(style="darkgrid")
 
-# STUDY RELATIONS BETWEEN PARAMETERS
+# Multi-plot grid
 # Histograms to study data distribution per parameter
-# Linear plots to see relationships between parameters
+# Lineplots and probability density function (KDE) plots
+# to study relationships between parameters
 
-plt.figure(1)
+plt.figure()
 g = sns.PairGrid(stats, hue="Outcome")
 g.map_diag(sns.histplot, kde=True)
 g.map_upper(sns.kdeplot, fill=True)
@@ -79,66 +80,69 @@ g.map_lower(sns.scatterplot)
 blue_patch = mpatches.Patch(color='blue', label='No diabetes')
 red_patch = mpatches.Patch(color='orange', label='Diabetes')
 g.add_legend(loc="upper right", handles=[blue_patch, red_patch])
-plt.suptitle("Dataset parameters distribution and relationships", y=1.01, fontsize=20)
-
+plt.suptitle("General data analysis - per parameter and between parameters", y=1, fontsize=18)
 plt.savefig("general_analysis.png", dpi=300, bbox_inches="tight");
 plt.show()
 
-"""
-From histograms we see that diabetes is mainly related with:
-- High glucose levels
+# Correlation matrix
+plt.figure()
+matrix = stats.corr().round(1)
+sns.heatmap(matrix, annot=True)
+plt.title('Data Correlation Matrix', fontsize=18)
+plt.savefig("corr_matrix.png", dpi=300, bbox_inches="tight");
+plt.show()
 
-From linear plots we see linear relationships between: 
-- BMI and Skin Thickness
-- Glucose and Insulin
-"""
-
-# Subplots with histograms (comparison for the different parameters between diabetes and no diabetes)
 col=stats.columns
+stats.Outcome = stats.Outcome.astype('category')
 
-plt.figure(2)
-fig2, axes2 = plt.subplots(nrows=2, ncols=4, figsize=(20,10))
+# Histograms
+plt.figure()
+fig2, axes2 = plt.subplots(nrows=4, ncols=2, figsize=(20,10))
 axes2 = fig2.add_subplot()
 plt.tight_layout()
 for i in range(len(col)-1):
-    plt.subplot(2,4,i+1)
-    sns.distplot(stats[stats.Outcome == 0][col[i]], label='No diabetes')
-    sns.distplot(stats[stats.Outcome == 1][col[i]], label='Diabetes')       
-plt.subplot(2,4,4).legend(loc='upper right')
-plt.suptitle("Dataset parameters distribution", y=1.01, fontsize=20)
+    for outcome in stats.Outcome.cat.categories:
+        plt.subplot(2,4,i+1)
+        sns.distplot(stats[stats.Outcome == outcome][col[i]])
+plt.subplot(2,4,4).legend(['No diabetes', 'Diabetes'], loc='upper right')
+plt.suptitle("Data analysis per parameter and diagnosis - histograms", y=1, fontsize=18)
 fig2.tight_layout(pad=1.2)
-plt.savefig("distribution.png", dpi=300, bbox_inches="tight");
+plt.savefig("histograms.png", dpi=300, bbox_inches="tight");
 plt.show()
 
-"""
-From histograms we see that diabetes is mainly related with:
-- High glucose levels
-"""
-
-"""
+# Boxplots 
 plt.figure(4)
-fig4, ax4 = plt.subplots(nrows=4, ncols=2, figsize=(10,20))
+fig4, ax4 = plt.subplots(nrows=2, ncols=4, figsize=(12,10))
 ax4 = fig4.add_subplot()
 plt.tight_layout()
-for i in range(len(col)):
-    plt.subplot(4,2,i+1)
-    sns.boxplot(data=stats, x='Outcome', y=col[i])       
+for i in range(len(col)-1):
+    plt.subplot(2,4,i+1)
+    sns.boxplot(data=stats, x='Outcome', y=col[i])
+    plt.xticks([0, 1], ['No diabetes', 'Diabetes'])
+    plt.xlabel('Diagnosis')    
+plt.suptitle("Data analysis per parameter and diagnosis - boxplots", y=1, fontsize=18)
+fig4.tight_layout(pad=1.2)
+plt.savefig("boxplots.png", dpi=300, bbox_inches="tight");
 plt.show(fig4)
 
+# Violinplots
 plt.figure(5)
-fig5, ax5 = plt.subplots(nrows=4, ncols=2, figsize=(10,20))
+fig5, ax5 = plt.subplots(nrows=2, ncols=4, figsize=(12,10))
 ax5 = fig5.add_subplot()
 plt.tight_layout()
-for i in range(len(col)):
-    plt.subplot(4,2,i+1)
-    sns.violinplot(data=stats, x='Outcome', y=col[i])       
-plt.show(5)
-"""
+for i in range(len(col)-1):
+    plt.subplot(2,4,i+1)
+    sns.violinplot(data=stats, x='Outcome', y=col[i]) 
+    plt.xticks([0, 1], ['No diabetes', 'Diabetes'])
+    plt.xlabel('Diagnosis') 
+plt.suptitle("Data analysis per parameter and diagnosis - violinplots", y=1, fontsize=18)
+fig5.tight_layout(pad=1.2)
+plt.savefig("violinplots.png", dpi=300, bbox_inches="tight");      
+plt.show(fig5)
 
-# STUDY GLUCOSE
+# STUDYING GLUCOSE
 # Histogram
-"""
-plt.figure(2)
+plt.figure(7)
 list1 = list()
 mylabels=list()
 for outcome in stats.Outcome.cat.categories:
@@ -148,29 +152,25 @@ for outcome in stats.Outcome.cat.categories:
 mylabels[0] = 'No diabetes'
 mylabels[1] = 'Diabetes'
 h = plt.hist(list1, bins=30, stacked=True, label = mylabels)
-plt.legend()
-plt.title('Glucose distribution')
+plt.legend(loc="upper left")
+plt.title('Blood glucose levels per diagnosis')
+plt.savefig("glucose_distribution.png", dpi=300, bbox_inches="tight");  
 plt.show(h)
 # most people has glucose~100
 
-# Glucose expressed in terms of age
-# Linear model plot
-plt.figure(3)
-vis1 = sns.lmplot(data=stats, x='Age', y='Glucose', hue='Outcome', size=5)
-plt.title('Glucose VS Age')
-plt.show(vis1)
-# The higher the age, the higher the glucose levels.
-# The diabetes outcome seems to be related with higher glucose levels
 
-# Kdeplot - heatmap
-plt.figure(4)
-k1=sns.kdeplot(stats.Age, stats.Glucose, shade=True, shade_lowest=False, cmap='Reds')
-k1b=sns.kdeplot(stats.Age, stats.Glucose, shade=True, cmap='Reds')
-plt.title('Glucose VS Age')
-plt.show()
-# The higher the age, the higher the glucose levels.
 """
+CONCLUSIONS
 
+COMPARING PARAMETERS we see linear relationships between: 
+- Insulin and Skin Thickness
+- BMI and Skin Thickness
+- Glucose and Insulin
+
+STUDYING THE RELATIONSHIP BETWEEN DIABETES AND EACH PARAMETER, 
+we can conclude that diabetes is mainly related with:
+- High glucose levels
+"""
 
 
 
